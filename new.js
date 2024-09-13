cfd6ed638f3f01f4197521344cd77841515cd951
AMIT KUMAR <153279290+iim-amit@users.noreply.github.com>
2024-09-14 02:15:54 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,11 @@
+let typed= new Typed(".text",{
+    strings:[" Frontend developer " ,"Web Developer","Sports Player"],
+    typeSpeed: 10,
+    backSpeed: 50,
+    backDelay: 100,
+    loop: true
+});
+
+
+
+
